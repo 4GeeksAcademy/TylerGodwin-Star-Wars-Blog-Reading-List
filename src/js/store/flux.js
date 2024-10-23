e480ddc9c52838f84595a6d3be179058e20dc37a
Tgodwin94 <tyler.godwin94@gmail.com>
2024-10-23 20:41:33 +0000

    Finished

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,158 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+    return {
+        store: {
+            people: JSON.parse(localStorage.getItem("people")) || [],
+            peopleInfo: JSON.parse(localStorage.getItem("peopleInfo")) || {},
+            planets: JSON.parse(localStorage.getItem("planets")) || [],
+            planetsInfo: JSON.parse(localStorage.getItem("planetsInfo")) || {},
+            vehicles: JSON.parse(localStorage.getItem("vehicles")) || [],
+            vehiclesInfo: JSON.parse(localStorage.getItem("vehiclesInfo")) || {},
+            favorites: JSON.parse(localStorage.getItem("favorites")) || []
+        },
+        actions: {
+            getPeople: async () => {
+                const store = getStore();
+                if (store.people.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/people?page=1&limit=83", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ people: data.results });
+                        localStorage.setItem("people", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            getPeopleInfo: async (id) => {
+                const store = getStore();
+                if (!store.peopleInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/people/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ peopleInfo: { ...store.peopleInfo, [id]: data.result.properties } });
+                        localStorage.setItem("peopleInfo", JSON.stringify({ ...store.peopleInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            getPlanets: async () => {
+                const store = getStore();
+                if (store.planets.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/planets?page=1&limit=60", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ planets: data.results });
+                        localStorage.setItem("planets", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            getPlanetsInfo: async (id) => {
+                const store = getStore();
+                if (!store.planetsInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/planets/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ planetsInfo: { ...store.planetsInfo, [id]: data.result.properties } });
+                        localStorage.setItem("planetsInfo", JSON.stringify({ ...store.planetsInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            getVehicles: async () => {
+                const store = getStore();
+                if (store.vehicles.length === 0) {
+                    try {
+                        const resp = await fetch("https://www.swapi.tech/api/vehicles?page=1&limit=76", {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ vehicles: data.results });
+                        localStorage.setItem("vehicles", JSON.stringify(data.results));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            getVehiclesInfo: async (id) => {
+                const store = getStore();
+                if (!store.vehiclesInfo[id]) {
+                    try {
+                        const resp = await fetch(`https://www.swapi.tech/api/vehicles/${id}`, {
+                            method: "GET",
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        });
+                        if (!resp.ok) {
+                            throw new Error(`error status: ${resp.status}`);
+                        }
+                        let data = await resp.json();
+                        setStore({ vehiclesInfo: { ...store.vehiclesInfo, [id]: data.result.properties } });
+                        localStorage.setItem("vehiclesInfo", JSON.stringify({ ...store.vehiclesInfo, [id]: data.result.properties }));
+                    } catch (error) {
+                        console.error("Error");
+                    }
+                }
+            },
+            addFavorite: (type, id, name) => {
+                const store = getStore();
+                const url = `/details/${type}/${id}`;
+                if (!store.favorites.find(fav => fav.name === name)) {
+                    const newFavorites = [...store.favorites, { type, id, name, url }];
+                    setStore({ favorites: newFavorites });
+                    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+                }
+            },
+            removeFavorite: (name) => {
+                const store = getStore();
+                const newFavorites = store.favorites.filter(fav => fav.name !== name);
+                setStore({ favorites: newFavorites });
+                localStorage.setItem("favorites", JSON.stringify(newFavorites));
+            }
+        }
+    };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Card } from "../component/cardCharacters";
+import { CardPlanets } from "../component/cardPlanets";
+import { CardVehicles } from "../component/cardVehicles";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store } = useContext(Context);
+
+    const eachCharacter = store.people.map((people, index) => (
+        <Card key={index} people={people} />
+    ));
+
+    const eachPlanet = store.planets.map((planet, index) => (
+        <CardPlanets key={index} planet={planet} />
+    ));
+
+    const eachVehicle = store.vehicles.map((vehicle, index) => (
+        <CardVehicles key={index} vehicle={vehicle} />
+    ));
+
+    return (
+        <>
+            <div className="m-5">
+                <h1>Characters</h1>
+                <div className="scrollmenu d-flex">
+                    {eachCharacter}
+                </div>
+            </div>
+            <div className="m-5">
+                <h1>Planets</h1>
+                <div className="scrollmenu d-flex">
+                    {eachPlanet}
+                </div>
+            </div>
+            <div className="m-5">
+                <h1>Vehicles</h1>
+                <div className="scrollmenu d-flex">
+                    {eachVehicle}
+                </div>
+            </div>
+        </>
+    );
+};
